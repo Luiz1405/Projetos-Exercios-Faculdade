1fa479d57dc1e6d3a134c8bf7834313048198cf1
Luiz Augusto de Souza Kubaszewski <116366008+Luiz1405@users.noreply.github.com>
2024-09-02 20:35:35 -0300

    Add files via upload

---
+++ Exercicios/Gerenciamento de Supermercado/src/ParametrosSimulacao.java
@@ -0,0 +1,5 @@
+public class ParametrosSimulacao {
+    public static double VALOR_COMPRA_MIN = 10;
+    public static double VALOR_COMPRA_MAX = 5000;
+    public static double PROBABILIDADE_ATENDIMENTO = 0.5;
+}
