0f22e059ceb99a3c7c87a9b7673c04f8b5177f2a
Luiz Augusto de Souza Kubaszewski <116366008+Luiz1405@users.noreply.github.com>
2024-09-07 11:17:36 -0300

    Delete Exercicios/Gerenciamento de Supermercado directory

--- Exercicios/Gerenciamento de Supermercado/src/ParametrosSimulacao.java
+++
@@ -1,5 +0,0 @@
-public class ParametrosSimulacao {
-    public static double VALOR_COMPRA_MIN = 10;
-    public static double VALOR_COMPRA_MAX = 5000;
-    public static double PROBABILIDADE_ATENDIMENTO = 0.5;
-}
